9eb9fb1ef7c464c2b8145dab0e2e2729e96fc897
Tina Ye <tinaye@berkeley.edu>
2018-02-05 21:41:38 -0800

    Create drawing-new-colors.js

---
+++ drawing-new-colors.js
@@ -0,0 +1,30 @@
+function setup() { 
+  createCanvas(400, 400);
+	colorMode(RGB);
+	
+} 
+
+var diameter = 10;
+
+function draw() { 
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, diameter);
+  }
+}
+
+function keyPressed() {
+  print(key);
+  if (key == 'R') {
+    fill(236, 95, 105);
+  } else if (key == 'G') {
+    fill(175, 253, 191);
+  } else if (key == 'B') {
+    fill(62, 162, 244); 
+  } else if (key == 'O') {
+		fill(251, 125, 75);
+	} else if (key == 'Y') {
+		fill(251, 210, 75);
+	} else if (key == 'P') {
+		fill(238, 170, 253);
+	}
+}
